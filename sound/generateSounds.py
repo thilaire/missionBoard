ebbf307560213bb50c1ab0f7593a13685c1be595
Thibault Hilaire <thibault.hilaire@lip6.fr>
2019-01-06 16:48:49 +0100

    script to generate the speech (french and english, but can be easily translated into any language). To add a new language, fill the infos in the generateSounds.py script

---
+++ sound/generateSounds.py
@@ -0,0 +1,44 @@
+# coding=utf-8
+
+"""Script to generate sound
+It requires the `say` command (with the default dictionary on my Mac, they should exist in GNUstep speech engine)
+
+To add a new language, add translated sentences in the dictionaries (in sounds list), and run the script
+(the folder `generated/{lang}` should exist)
+Configure the sound path in config.py
+"""
+
+from subprocess import Popen, PIPE
+
+# languages used for the generation
+languages = ['en', 'fr']
+
+# dictionary of voices
+voices = {'fr': 'Thomas', 'en': 'Alex'}
+
+# list of sounds to generate (fileName, sentence to pronounce)
+# `$` is a silent (replaced by '[[slnc 10]]')
+sounds = [
+	('phase1', 'phase $ 1'),
+	('Initialization', {'fr': 'Initialisation', 'en': 'Initialization'}),
+	('phase2', 'phase $ 2'),
+	('phase3', 'phase $ 3'),
+	('phase1engaged', {'fr': 'Phase $ 1 $engagée', 'en': 'Phase $ 1 $ engaged'}),
+	('phase2engaged', {'fr': 'Phase $ 2 $ engagée', 'en': 'Phase $ 2 $ engaged'}),
+	('phase3engaged', {'fr': 'Phase $ 3 $ engagée, $$$$ Début du compte à rebours',
+	'en': 'Phase $ 3 $ engaged, $$$$ the countdown starts'}),
+]
+# add the numbers
+sounds.extend(list((str(x), str(x)) for x in range(10)))
+
+# iter all the languages
+for lang in languages:
+	# iter all the sounds
+	for name, sentence in sounds:
+		# get the translated sentence and insert the silences
+		if isinstance(sentence, dict):
+			sentence = sentence[lang]
+		sentence = sentence.replace('$', '[[slnc 10]]')
+		# generate the sound
+		Popen(['say', '-v', voices[lang], '"'+sentence+'"', '--data-format=LEI16@22050', '-o', 'generated/'+lang+'/'+name+'.wav'], stdout=PIPE)
+
