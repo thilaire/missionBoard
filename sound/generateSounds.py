b89db6ec5b620956163851f6d3e7614403b2b2b9
Thibault Hilaire <thibault.hilaire@lip6.fr>
2020-03-09 10:23:43 +0100

    PEP8 cleaning

--- sound/generateSounds.py
+++ sound/generateSounds.py
@@ -12,7 +12,7 @@ Configure the sound path in config.py
 """
 
 from subprocess import call
-from time import sleep
+
 # languages used for the generation
 languages = ['en', 'fr']
 
@@ -46,7 +46,8 @@ for lang in languages:
 		# generate the sound
 		# we cannot fully rely on `say` because the wav file output cannot be read by pygame (the wav format is `unexpected`)
 		# so we first ccreate a aiff file, and then convert it into a correct wav file
-		# Popen(['say', '-v', voices[lang], '"'+sentence+'"', '--data-format=LEI16@22050', '--channels=2', '-o', 'generated/'+lang+'/'+name+'.wav'], stdout=PIPE)
+		# Popen(['say', '-v', voices[lang], '"'+sentence+'"', '--data-format=LEI16@22050', \
+		# '--channels=2', '-o', 'generated/'+lang+'/'+name+'.wav'], stdout=PIPE)
 		call(['say', '-v', voices[lang], '"' + sentence + '"', '--channels=2', '-o', 'generated/temp'])
 		call(['afconvert', '-f', 'WAVE', '-d', 'LEI16', 'generated/temp.aiff', 'generated/'+lang+'/'+name+'.wav'])
 		call(['rm', 'generated/temp.aiff'])
